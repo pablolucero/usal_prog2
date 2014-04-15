4e3c88251d320947aa3763c7ba9ce6183d98fb3d
Pablo Lucero <plucero@tekgenesis.com>
2014-04-15 17:25:57 -0300

    add DetalleDeFactura

---
+++ ejercitaciones1/DetalleDeFactura.java
@@ -0,0 +1,20 @@
+package ejercitaciones1;
+
+/**
+ * Generar un programa que permita indicar en un array, precios de productos
+ * En otro array, descripciones de los productos respectivos
+ * En otra variable, el porcentaje de descuento
+ * El programa debe calcular el total menos el descuento, y presentar la siguiente salida:
+ * Resumen de venta:
+ * Producto 1    $23
+ * Producto 2    $25
+ *
+ * Total:           $48
+ * Descuento:   10%
+ * Total final:    $39,2
+ */
+public class DetalleDeFactura {
+    public static void main(String[] args) {
+
+    }
+}
