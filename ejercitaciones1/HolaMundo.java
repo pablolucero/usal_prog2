13f1466f4c4a7b53a058380ed081c48d1db788b3
Pablo Lucero <plucero@tekgenesis.com>
2014-04-15 13:12:44 -0300

    add Sumatoria

--- ejercitaciones1/HolaMundo.java
+++ ejercitaciones1/HolaMundo.java
@@ -1,7 +1,10 @@
 package ejercitaciones1;
 
+/**
+ * Generar un programa que presente en pantalla el texto “Hola Mundo!”
+ */
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo!");
     }
 }
--- ejercitaciones1/Sumatoria.java
+++ ejercitaciones1/Sumatoria.java
@@ -1,7 +1,16 @@
 package ejercitaciones1;
 
+/**
+ * Generar un programa que sume los números 5 y 10 y muestre el resultado en pantalla,
+ * almacenando todos los datos en variables
+ */
 public class Sumatoria {
     public static void main(String[] args) {
-        System.out.println("lalalalalal");
+        int five = 5;
+        int ten = 10;
+
+        int sum = five + ten;
+
+        System.out.println(sum);
     }
 }
