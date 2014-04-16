9eb55ca42cc3c1f1cdc6fad240270de17b6d061a
Pablo Lucero <plucero@tekgenesis.com>
2014-04-16 17:07:53 -0300

    first implementation of DetalleDeFactura

--- ejercitaciones1/DetalleDeFactura.java
+++ ejercitaciones1/DetalleDeFactura.java
@@ -10,11 +10,37 @@ package ejercitaciones1;
  * Producto 2    $25
  *
  * Total:           $48
- * Descuento:   10%
- * Total final:    $39,2
+ * Descuento:       10%
+ * Total final:     $39,2
  */
 public class DetalleDeFactura {
     public static void main(String[] args) {
 
+        int numDeProductos = 3;
+
+        double[] precios;
+        String[] descripciones;
+        double porcentajeDescuento = 10;
+        double total = 0;
+
+        // TODO: leer data por teclado
+        precios = new double[]{10, 20, 30};
+        descripciones = new String[]{"Producto 1", "Producto 2", "Producto 3"};
+
+        System.out.println("Resumen de venta:");
+        for (int i = 0; i < numDeProductos; i++) {
+            System.out.println(descripciones[i] + "\t\t\t $" + precios[i]);
+            total = total + precios[i];
+        }
+
+        System.out.println("Total:\t\t\t\t $" + total);
+        System.out.println("Descuento:\t\t\t " + porcentajeDescuento + "%");
+        double totalFinal = aplicarDescuento(total, porcentajeDescuento);
+        System.out.println("Total final:\t\t $" + totalFinal);
+
+    }
+
+    public static double aplicarDescuento(double monto, double porcentaje) {
+        return (monto * (1 - (porcentaje / 100)));
     }
 }
